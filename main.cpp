88f1568dc4af8a9755f5c4b19a2b683cd9257cc7
JARRIGE Romain <Romain.Jarrige@bordeaux-inp.fr>
2024-10-16 10:32:11 +0200

    Interuption bouton

--- main.cpp
+++ main.cpp
@@ -7,37 +7,28 @@
 
 // Blinking rate in milliseconds
 #define BLINKING_RATE     500ms
-//#define I2C_SDA P73
-//#define I2C_SCL P75
-
-//I2C i2c(I2C_SDA, I2C_SCL);
-//const int addr7bit = 0x48;      // 7 bit I2C address
-//const int addr8bit = 0x48 << 1; // 8bit I2C address, 0x90
-
-int main()
-{
-    // Initialise the digital pin LED1 as an output
-
-    gpio_t sw;
-    gpio_init(&sw, BUTTON1);
 
 #ifdef LED1
     DigitalOut led(LED1);
 #else
     bool led;
 #endif
-    char cmd = 0;
+
+void button_handle();
+
+int main()
+{
+    InterruptIn button(BUTTON1);
+    button.rise(&button_handle);
 
     while (true) {
-        //i2c.read(addr8bit, &cmd, 2);
-        //printf ("%d\n", cmd);
-        // printf("Hello\n"); Test de communication carte lors de la première compile
-        led = !led;
         ThisThread::sleep_for(BLINKING_RATE);
-
-        int sw_value = gpio_read(&sw);
-        printf("Button: %d\n", sw_value);
     }
+}
+
+void button_handle() {
+    led = !led;
+    // printf("Button pressé\n");
 }
 
 
